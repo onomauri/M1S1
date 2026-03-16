45b27221889fa131bc4e55dfd8406c5a387e11ab
onomauri <onofremauro0@gmail.com>
2026-03-16 10:30:45 -0500

    test00

--- tests.py
+++ tests.py
@@ -2,4 +2,8 @@ import tkinter as tkinter
 from tkinter import simpledialog
 
 name=simpledialog.askstring("Producto","Ingrese nombre del producto: ")
+price=simpledialog.askfloat("Precio","Ingrese el valor del producto: ")
+amount=simpledialog.askinteger("Cantidad","Ingrese la cantidad")
+total=(price*amount)
 print(name)
+print(total)
