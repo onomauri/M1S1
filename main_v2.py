3cec13cdd91408b5cbf642c956c70d52becf46fa
onomauri <onofremauro0@gmail.com>
2026-03-17 06:49:46 -0500

    test02

--- main_v2.py
+++ main_v2.py
@@ -1,21 +1,25 @@
 import tkinter as tkinter
 from tkinter import simpledialog
+from tkinter import messagebox
+
 
 print("Bienvenido al registro de ventas!\n")
+
 
 all_data = []
 end = "no"
 
+
 def sales_log():
-    name=simpledialog.askstring("Ingrese nombre del producto: ")
-    price=float(input("Ingrese el precio del producto: "))
+    name=simpledialog.askstring("producto","Ingrese nombre del producto: ")
+    price=simpledialog.askfloat("precio","Ingrese el precio del producto: ")
     while price <= 0:
-        price=float(input("El valor debe ser superior a 0.\nIngrese cantidad nuevamente "))
-    amount=int(input("Ingrese cantidad: "))
+        price=simpledialog.askfloat("precio","El valor debe ser superior a 0.\nIngrese cantidad nuevamente ")
+    amount=simpledialog.askinteger("cantidad","Ingrese cantidad: ")
     while amount <= 0:
-        amount=int(input("Debe de ingresar un minimo de '1' producto.\nIngrese la cantidad nuevamente: "))
+        amount=simpledialog.askinteger("cantidad,""Debe de ingresar un minimo de '1' producto.\nIngrese la cantidad nuevamente: ")
     total=(price*amount)
-    print(f"Subtotal:{total}")
+    messagebox.showinfo("subtotal",f"Subtotal:{total}")
     s_log={
         "name": name,
         "price": price,
@@ -24,21 +28,23 @@ def sales_log():
     }
     all_data.append(s_log)
 
+reporte=""
+
 def summary():
-    print("\nResumen de ventas.\n")
     for i in all_data:
-        print(f"producto: {i['name']}\n\nvalor unitario: {i['price']}\ncantidad: {i['amount']}\n\ntotal: {i['total']}.\n\n") 
+        reporte += (f"producto: {i['name']}\n\nvalor unitario: {i['price']}\ncantidad: {i['amount']}\n\ntotal: {i['total']}.\n\n")
+
 
 def total_s():
-    f_total= sum(i["total"] for i in all_data) 
+    f_total= sum(i["total"] for i in all_data)
     print(f"El total recaudado en las ventas es: {f_total}")
-    
-
+   
 try:
     while end != "si":
         sales_log()
         end=input("Desea finalizar de digitar?: ")
     summary()
+    messagebox.showinfo("Resumen de ventas", reporte)
     total_s()
     print("Gracias por usar el sistema de registro!")
 except:
--- tests.py
+++ tests.py
@@ -2,9 +2,32 @@ import tkinter as tkinter
 from tkinter import simpledialog
 from tkinter import messagebox
 
-name=simpledialog.askstring("Producto","Ingrese nombre del producto: ")
-price=simpledialog.askfloat("Precio","Ingrese el valor del producto: ")
-amount=simpledialog.askinteger("Cantidad","Ingrese la cantidad")
+#name=simpledialog.askstring("Producto","Ingrese nombre del producto: ")
+#price=simpledialog.askfloat("Precio","Ingrese el valor del producto: ")
+#amount=simpledialog.askinteger("Cantidad","Ingrese la cantidad")
+#total=(price*amount)
+#print(name)
+#messagebox.showinfo("Cantidad", f"El total es: {total}")
+
+all_data = []
+
+name=simpledialog.askstring("producto","Ingrese nombre del producto: ")
+price=simpledialog.askfloat("precio","Ingrese el precio del producto: ")
+while price <= 0:
+    price=simpledialog.askfloat("precio","El valor debe ser superior a 0.\nIngrese cantidad nuevamente ")
+amount=simpledialog.askinteger("cantidad","Ingrese cantidad: ")
+while amount <= 0:
+    amount=simpledialog.askinteger("cantidad,""Debe de ingresar un minimo de '1' producto.\nIngrese la cantidad nuevamente: ")
 total=(price*amount)
-print(name)
-messagebox.showinfo("Cantidad", f"El total es: {total}")
+messagebox.showinfo("subtotal",f"Subtotal:{total}")
+s_log={
+    "name": name,
+    "price": price,
+    "amount":amount,
+    "total":total
+}
+all_data.append(s_log)
+reporte=""
+for i in all_data:
+        reporte += (f"producto: {i['name']}\n\nvalor unitario: {i['price']}\ncantidad: {i['amount']}\n\ntotal: {i['total']}.\n\n")
+print(reporte)
